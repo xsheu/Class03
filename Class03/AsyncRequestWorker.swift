f9f897105a18f34fd9671ba288771434c7c10a65
Guang Yih Sheu <xsheu@hotmail.com>
2019-06-03 17:40:42 +0800

    2019 6/3 17:40

--- Class03/AsyncRequestWorker.swift
+++ Class03/AsyncRequestWorker.swift
@@ -28,8 +28,6 @@ class AsyncRequestWorker {
         
         let task = session.dataTask(with: request, completionHandler: { (data, response, error) in
             
-            
-            
             let httpResponse = response as! HTTPURLResponse
             let statusCode = httpResponse.statusCode
             
@@ -38,12 +36,11 @@ class AsyncRequestWorker {
                 let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
                 let responseString = String( dataString! )
                 
-                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "response.received"), object: self, userInfo: ["response": responseString])
-                
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "response.received"), object: self, userInfo: ["aaa": responseString])
+
                 self.reponseDelegate?.receviedReponse(self, responseString: responseString, tag: tag)
             }
         })
         task.resume()
     }
-
 }
---
+++ Class03/MyMapViewController.swift
@@ -0,0 +1,69 @@
+//
+//  MyMapViewController.swift
+//  Class03
+//
+//  Created by L20102 on 2019/6/3.
+//  Copyright © 2019 L20102. All rights reserved.
+//
+
+import UIKit
+
+import MapKit
+import CoreLocation
+
+class MyMapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    var locationManager : CLLocationManager!
+    
+    let regionRadius : CLLocationDistance = 10000
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        if ( CLLocationManager.locationServicesEnabled() ){
+            locationManager.startUpdatingLocation()
+        }
+        
+        
+        myMapView.delegate = self
+        myMapView.showsUserLocation = true
+        
+        
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let loc : CLLocation = locations[0] as CLLocation
+        
+        DispatchQueue.main.async {
+            self.centerLocation(location: loc)
+        }
+    }
+    
+
+    func centerLocation(location : CLLocation){
+        
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        
+        DispatchQueue.main.async {
+            self.myMapView.setRegion(region, animated: true)
+            
+        }
+        
+        
+    }
+
+
+}
